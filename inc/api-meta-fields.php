7b89c9d0a39de2fe2a276af6d843892cc5ba22a6
Ricard Torres <torres.rick@gmail.com>
2020-12-28 23:01:38 +0100

    Update api-meta-fields.php

--- inc/api-meta-fields.php
+++ inc/api-meta-fields.php
@@ -6,7 +6,6 @@
 add_action( 'rest_api_init', 'create_api_posts_meta_field' );
 
 function create_api_posts_meta_field() {
-    // register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
     register_rest_field( 'post', 'post-meta-fields', array(
            'get_callback'    => 'get_post_meta_for_api',
            'schema'          => null,
@@ -18,6 +17,6 @@ function get_post_meta_for_api( $object ) {
     //get the id of the post object array
     $post_id = $object['id'];
 
-    //return the post meta
-    return get_post_meta( $post_id );
+    // return the post meta
+    return array(get_post_meta( $post_id ), 'my test 123');
 }
